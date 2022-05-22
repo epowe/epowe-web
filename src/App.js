3ed3cd0c29787463bf0f10cc5045af7d8156c8af
DongHyeon <skehdgus401@gmail.com>
2022-05-23 02:14:23 +0900

    feat: 폰트 적용, 라우터 적용

--- src/App.js
+++ src/App.js
@@ -4,12 +4,9 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      asdfadsf
-      <Routes>
-        <Route path="/M" element={<MainPage />}></Route>
-      </Routes>
-    </div>
+    <Routes>
+      <Route path="/" element={<MainPage />} />
+    </Routes>
   );
 }
 
--- src/views/MainPage.js
+++ src/views/MainPage.js
@@ -1,13 +1,19 @@
 import React, { useEffect, useContext } from "react";
 import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
+import HeaderLogoI from "../images/HeaderLogo.png";
 
 const MainPage = () => {
   const navigate = useNavigate();
-
+  const onClickLogo = () => {
+    navigate("/");
+  };
   return (
     <Container>
-      <div>sdfasdfasdfsdfds</div>
+      <Header>
+        <HeaderLogoImage src={HeaderLogoI} onClick={onClickLogo} />
+        <HeaderLogo>Epowe</HeaderLogo>
+      </Header>
     </Container>
   );
 };
@@ -16,8 +22,33 @@ const Container = styled.div`
   position: relative;
   width: 100vw;
   height: 100vh;
-  background-color: red;
+  background-color: #ffffff;
+  overflow: hidden;
+`;
+const Header = styled.div`
+  position: relative;
+  left: 23px;
+  top: 19px;
+  width: 100vw;
+  height: 100vh;
+  background-color: #ffffff;
   overflow: hidden;
 `;
 
+const HeaderLogoImage = styled.img`
+  width: 51px;
+  height: 51px;
+  cursor: pointer;
+`;
+
+const HeaderLogo = styled.div`
+  font-family: Montserrat-SemiBold;
+  font-size: 22px;
+  color: #6c63ff;
+  cursor: pointer;
+`;
+const UserContainer = styled.div`
+  position: absolute;
+  right: 0px;
+`;
 export default MainPage;
