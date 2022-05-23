d7f6621ca6c704c6bdaffa6f5c440bfdb63af32f
DongHyeon <skehdgus401@gmail.com>
2022-05-23 23:48:35 +0900

    chore: css 수정

--- src/views/MainPage.js
+++ src/views/MainPage.js
@@ -12,6 +12,7 @@ const MainPage = () => {
     <Container>
       <Header>
         <HeaderLogoImage src={HeaderLogoI} onClick={onClickLogo} />
+        <Cover></Cover>
         <HeaderLogo>Epowe</HeaderLogo>
       </Header>
     </Container>
@@ -30,7 +31,7 @@ const Header = styled.div`
   left: 23px;
   top: 19px;
   width: 100vw;
-  height: 100vh;
+  height: 85px;
   background-color: #ffffff;
   overflow: hidden;
 `;
@@ -43,12 +44,17 @@ const HeaderLogoImage = styled.img`
 
 const HeaderLogo = styled.div`
   font-family: Montserrat-SemiBold;
+  display: inline-block;
+  position: relative;
   font-size: 22px;
   color: #6c63ff;
   cursor: pointer;
+  margin-left: 19px;
+  margin-top: 20px;
 `;
-const UserContainer = styled.div`
-  position: absolute;
-  right: 0px;
+
+const Cover = styled.div`
+  display: inline-block;
+  padding-bottom: 19px;
 `;
 export default MainPage;
