f5d389c1facb5ffc6349fcffb4f760c263672c72
seoyoonOh <5happy1004kr@naver.com>
2022-07-07 01:02:16 +0900

    fix: 회원가입 페이지 수정

--- src/views/Register.js
+++ src/views/Register.js
@@ -1,16 +1,34 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import Header from "./Header.js";
 
 const Register = () => {
+  const navigate = useNavigate();
+  const addressRef = useRef();
+
+  const onRegister = () => {
+    if (addressRef.current.value !== "") {
+      //회원가입 처리하기
+      navigate('/interview');
+    } else {
+      alert('사는 지역을 입력해주세요.') //-> modal로 바꾸기
+    }
+  };
+
   return (
     <>
       <Header/>
       <BodyContainer>
         <ButtonContainer>
           <Title>회원가입</Title>
-          <Input placeholder="사는 지역을 입력해주세요." />
-          <Button>
+          <ProfileContainer>
+            <Span><Image src="https://www.business2community.com/wp-content/uploads/2017/08/blank-profile-picture-973460_640.png"/></Span>
+            <Span>홍길동</Span>
+            <Span>example@naver.com</Span>
+          </ProfileContainer>
+          <Input ref={addressRef} placeholder="사는 지역을 입력해주세요." />
+          <Button onClick={onRegister}>
             회원가입하기
           </Button>   
         </ButtonContainer>
@@ -27,6 +45,25 @@ const BodyContainer = styled.div`
   align-items: center;
 `;
 
+const ProfileContainer = styled.div`
+  margin: 1rem;
+  display:flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  align-items: center;
+`;
+
+const Span = styled.span`
+  margin-top: 1rem;
+  font-family: SCDream-Regular;
+`;
+
+const Image = styled.img`
+  width: 4rem;
+  height: 4rem;
+  border-radius: 50%;
+`;
+
 const ButtonContainer = styled.div`
   display: flex;
   flex-direction: column;
@@ -36,7 +73,7 @@ const ButtonContainer = styled.div`
 
 const Title = styled.div`
   font-family: SCDream-Regular;
-  font-size: 24px;
+  font-size: 1.5rem;
   text-align: center;
   margin: 0;
 `;
@@ -46,27 +83,27 @@ const Button = styled.button`
   background: #6C63FF;
   color: white;
   width: 90%;
-  padding: 15px;
-  margin-top: 20vh;
-  border: none;
+  padding: 1rem;
+  margin-top: 1.5rem;
+  outline: none;
+  border: 0;
   border-radius: 50px;
   font-family: SCDream-Regular;
-  font-size: 15px;
+  font-size: 1rem;
   cursor: pointer;
   text-align: center;
 `;
 
 const Input = styled.input`
   box-sizing: border-box;
-  border: #6C63FF 2px solid;
+  border: #6C63FF 0.125rem solid;
   outline: none;
   width: 90%;
   height: 50px;
-  padding: 0 0 0 15px;
-  margin-top: 5vh;
+  padding: 0 0 0 1rem;
+  margin-top: 1rem;
   border-radius: 50px;
-  font-family: SCDream-Regular;
-  font-size: 15px;
+  font-size: 0.9rem;
 `;
 
 export default Register
