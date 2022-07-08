6a6060c549eec7b413baab0bfae75fd91ee32148
DongHyeon <skehdgus401@gmail.com>
2022-07-08 19:15:27 +0900

    fix: 로그인시 회원가입 페이지 이동

--- src/App.js
+++ src/App.js
@@ -10,10 +10,12 @@ import AppContext from "./AppContext";
 import axios from "axios";
 import ApiBaseURL from "./ApiBaseURL";
 import API from "./API";
+import Oauth2RedirectURL from "./Oauth2RedirectURI";
 
 function App() {
   const BASE_URL = ApiBaseURL;
-  axios.defaults.baseURL = BASE_URL;
+  // axios.defaults.baseURL = BASE_URL;
+  console.log(axios.defaults.baseURL);
   axios.defaults.withCredentials = false;
   const [isLogged, setIsLogged] = useState(
     localStorage.getItem("isLogged") === "true" ? true : false
@@ -22,31 +24,32 @@ function App() {
     isLogged,
     setIsLogged,
   };
-  async function getData(data) {
+
+  const authLogin = async () => {
     try {
       //응답 성공
-      const response = await axios.get("oauth2/redirect", {
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-      console.log(response);
-      console.log("응답 성공");
+      axios
+        .get("http://localhost:3000/oauth2/redirect", {
+          withCredentials: true,
+        })
+        .then((response) => {
+          console.log(response.data);
+        });
     } catch (error) {
       //응답 실패
       console.error(error);
-      console.log("응답 실패");
+      console.log("응답 실패glalalal");
     }
-  }
+  };
   useEffect(() => {
-    getData();
+    authLogin();
   }, []);
 
   return (
     <Routes>
       <Route path="/" element={<MainPage />} />
       <Route path="/interview" element={<Interview />} />
-      {/* <Route path="/oauth2/redirect" element={<Register />} /> */}
+      <Route path="/oauth2/redirect" element={<Register />} />
       <Route path="/interview/info" element={<InterviewInfo />} />
       <Route path="/interview/feedback" element={<Feedback />} />
     </Routes>
