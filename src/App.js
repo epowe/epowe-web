77873146d167a512360c7f1e372e31182b61df13
DongHyeon <skehdgus401@gmail.com>
2022-07-17 02:13:41 +0900

    feat: 사용자 주소 존재 여부 판별

--- src/App.js
+++ src/App.js
@@ -12,7 +12,8 @@ import QuestionList from "./views/QuestionList";
 import FeedbackDetail from "./views/FeedbackDetail.jsx";
 import axios from "axios";
 import ApiBaseURL from "./ApiBaseURL";
-import TokenProcess from "./TokenProcess";
+import { TokenProcess } from "./TokenProcess";
+import { API } from "./API";
 
 const App = () => {
   const BASE_URL = ApiBaseURL;
@@ -25,38 +26,34 @@ const App = () => {
     isLogged,
     setIsLogged,
   };
-  // const authLogin = async () => {
-  //   try {
-  //     //응답 성공
-  //     const response = await axios.get(
-  //       "http://localhost:3000/oauth2/redirect",
-  //       {
-  //         withCredentials: true,
-  //       }
-  //     );
-  //     if (response.status === 200) {
-  //       console.log("Okay");
-  //     }
-  //     console.log(response);
-  //     console.log("응답 성공");
-  //   } catch (error) {
-  //     //응답 실패
+  const [userName, setUserName] = useState("");
+  const [userEmail, setUserEmail] = useState("");
+  const [userAddress, setUserAddress] = useState("");
+  const [userProfile, setUserProfile] = useState("");
 
-  //     console.error(error);
-  //     console.log("응답 실패glalalal");
-  //   }
-  // };
-  // useEffect((location) => {
-  //   const params = new URLSearchParams(window.location.search);
-  //   let userToken = params.get("userToken");
+  const getUserAddress = async () => {
+    var result = await API.authAfterLogin();
+    if (result) {
+      setUserAddress(result.address);
+    } else {
+      console.log("사용자 데이터 잘 들어오지 않음");
+    }
+  };
 
-  //   let getParameter = (key) => {
-  //     return new URLSearchParams(window.location.search).get(key);
-  //   };
-  //   let userToken2 = getParameter("userToken");
-  //   console.log(userToken2);
-  //   // authLogin();
-  // }, []);
+  useEffect(() => {
+    if (localStorage.getItem("jwtToken")) {
+      getUserAddress();
+      if (userAddress) {
+        console.log("userAddress 존재");
+        console.log(userAddress);
+      }
+      else{
+        
+      }
+    } else {
+      console.log("JWT 발급 실패");
+    }
+  }, [userAddress]);
 
   return (
     <Routes>
--- src/TokenProcess.js
+++ src/TokenProcess.js
@@ -1,26 +1,10 @@
-// import React, { useEffect } from "react";
-// import { useParams } from "react-router";
-
-// function GoogleLoginRedirect() {
-//   const params = useParams();
-
-//   useEffect(() => {
-//     localStorage.clear();
-//     localStorage.setItem("token", params.token);
-//     console.log(params.token);
-//     window.location.replace("/");
-//   }, []);
-
-//   return <></>;
-// }
-
-// export default GoogleLoginRedirect;
 import { useLocation } from "react-router-dom";
 import queryString from "query-string";
 import React, { useEffect } from "react";
 import axios from "axios";
 import ApiBaseURL from "./ApiBaseURL";
-const GoogleLoginRedirect = ({ location }) => {
+
+export const TokenProcess = ({ location }) => {
   const params = new URLSearchParams(window.location.search);
   let userToken = params.get("userToken");
   let getParameter = (key) => {
@@ -28,33 +12,12 @@ const GoogleLoginRedirect = ({ location }) => {
   };
   let userToken2 = getParameter("userToken");
 
-  const authAfterLogin = async () => {
-    try {
-      //응답 성공
-      const response = await axios.get("http://localhost:3000/auth/info", {
-        headers: {
-          Authorization: `Bearer ${userToken2}`,
-          "Content-Type": "application/json",
-        },
-      });
-      if (response.status === 200) {
-        console.log("afterLogin api get 요청 성공");
-        console.log(response);
-        return response.data;
-      }
-      
-    } catch (error) {
-      console.error(error);
-      console.log("afterlogin 응답 실패");
-    }
-  };
-
   useEffect(() => {
     if (userToken2) {
       localStorage.clear();
       localStorage.setItem("jwtToken", userToken2);
       // axios.defaults.headers.common["Authorization"] = `Bearer ${userToken2}`;
-      authAfterLogin();
+      //authAfterLogin();
     } else {
       console.log("토큰을 못 받아옴");
     }
@@ -64,5 +27,4 @@ const GoogleLoginRedirect = ({ location }) => {
 
   return <></>;
 };
-/// 토큰이 들어오지 않으면 401로 
-export default GoogleLoginRedirect;
+/// 토큰이 들어오지 않으면 401로
