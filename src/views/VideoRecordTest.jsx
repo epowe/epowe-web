efd19c73f32ba01638facde504acdf426fda143a
DongHyeon <skehdgus401@gmail.com>
2022-07-25 16:44:59 +0900

    for: merge

--- src/views/VideoRecordTest.jsx
+++ src/views/VideoRecordTest.jsx
@@ -20,6 +20,17 @@ const VideoRecordTest = () => {
     }
   };
 
+  // 사용자에게 카메라 화면의 데이터를 요청
+  const constraints = {
+    audio: false,
+    video: true,
+  };
+
+  navigator.mediaDevices.getUserMedia(constraints).then(function(mediaStream) {
+    // 비디오 트랙을 포함한 MediaStream
+    console.log(mediaStream);
+  });
+
   return (
     <>
       <Header isLogin="true" />
