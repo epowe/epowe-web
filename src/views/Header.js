ab9f605a6927758b2b72561c4e957b9dfd53e87c
DongHyeon <skehdgus401@gmail.com>
2022-08-03 14:43:20 +0900

    feat: 주소 미입력후 헤더 프로필 사진 클릭시 마이페이지 접근 불가 메세지 추가

--- src/views/Header.js
+++ src/views/Header.js
@@ -61,7 +61,13 @@ const Header = ({ isLogin }) => {
         (window.location.pathname === "/") !== true ? (
           <SmallContainer>
             <SmallButton onClick={onLogout}>로그아웃</SmallButton>
-            <ProfileContainer onClick={(window.location.pathname === "/register" ? "" : () => navigate("/feedback"))}>
+            <ProfileContainer
+              onClick={
+                window.location.pathname === "/register"
+                  ? () => alert("마이페이지 접근 불가, 주소를 입력해주세요.")
+                  : () => navigate("/feedback")
+              }
+            >
               <Image src={userProfile} />
               <Span>{userName}</Span>
             </ProfileContainer>
