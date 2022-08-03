f44dc288f89889c64025d29074250c45e614bb8d
DongHyeon <skehdgus401@gmail.com>
2022-08-03 14:40:45 +0900

    fix: header 프로필 사진 클릭 오류 수정

--- src/views/Header.js
+++ src/views/Header.js
@@ -61,7 +61,7 @@ const Header = ({ isLogin }) => {
         (window.location.pathname === "/") !== true ? (
           <SmallContainer>
             <SmallButton onClick={onLogout}>로그아웃</SmallButton>
-            <ProfileContainer onClick={(window.location.pathname === "/register" ? "" : navigate("/feedback"))}>
+            <ProfileContainer onClick={(window.location.pathname === "/register" ? "" : () => navigate("/feedback"))}>
               <Image src={userProfile} />
               <Span>{userName}</Span>
             </ProfileContainer>
