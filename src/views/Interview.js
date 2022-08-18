0cfc060bdefc855f0b4a643262e5689d84c609c9
DongHyeon <skehdgus401@gmail.com>
2022-08-18 19:37:13 +0900

    fix: flask 서버 배포 테스트 셋팅 완료

--- src/views/Interview.js
+++ src/views/Interview.js
@@ -1,30 +1,73 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import Header from "./Header.js";
+import { API } from "../API.js";
 
 const Interview = () => {
   const navigate = useNavigate();
+
   const onClickStart = () => {
     navigate("/interview/info");
   };
 
+  const onClickFlaskPostTest = async () => {
+    var result = await API.useFlaskTestPost({
+      title: "제목입니다.",
+      question: "질문입니다.",
+      videoURL: "동영상 주소입니다.",
+    });
+    if (result) {
+      console.log("flask에 Post 완료");
+    } else {
+      console.log("flask Post 실패");
+      console.log(result);
+    }
+  };
+
+  const onClickFlaskGetTest = async () => {
+    var result = await API.useFlaskTestGet();
+    if (result) {
+      console.log("flask get 성공");
+      console.log(result);
+      if (result.dialectCount) {
+        console.log("사투리 사용횟수:" + result.dialectCount);
+      } else {
+        console.log("사용횟수 존재x");
+      }
+      if (result.intonation) {
+        console.log("억양 점수:" + result.intonation);
+      } else {
+        console.log("억양 점수 존재x");
+      }
+      if (result.speechRate) {
+        console.log("말의 빠르기 점수:" + result.speechRate);
+      } else {
+        console.log("말의 빠르기 존재x");
+      }
+      if (result.word) {
+        console.log("가장 많이 사용하는 단어:" + result.word);
+      } else {
+        console.log("가장 많이 사용하는 단어 존재x");
+      }
+    } else {
+      console.log("Flask get 실패");
+    }
+  };
   return (
     <>
-      <Header isLogin="true"/>
+      <Header isLogin="true" />
       <BodyContainer>
         <ButtonContainer>
-          <Button onClick={onClickStart}>
-            모의면접 시작하기
-          </Button>
-          <Button onClick={() => navigate("/feedback")}>
-            내 피드백 보기
-          </Button>   
+          {/* <Button onClick={onClickStart}>모의면접 시작하기</Button>
+          <Button onClick={() => navigate("/feedback")}>내 피드백 보기</Button> */}
+          <Button onClick={onClickFlaskPostTest()}>모의면접 시작하기</Button>
+          <Button onClick={onClickFlaskGetTest()}>내 피드백 보기</Button>
         </ButtonContainer>
       </BodyContainer>
     </>
-  )
-}
+  );
+};
 
 const BodyContainer = styled.div`
   position: fixed;
@@ -48,7 +91,7 @@ const ButtonContainer = styled.div`
 
 const Button = styled.button`
   box-sizing: border-box;
-  background: #6C63FF;
+  background: #6c63ff;
   color: white;
   width: 100%;
   padding: 0.8rem;
@@ -65,4 +108,4 @@ const Button = styled.button`
   }
 `;
 
-export default Interview
+export default Interview;
