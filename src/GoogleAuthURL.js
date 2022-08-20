29777f47b6043f753a1eb1ca21d0f6ebad43d207
HiBixby <fpssj@naver.com>
2022-08-20 23:21:59 +0900

    ⚗️ URL 하드코딩으로 변경

--- src/GoogleAuthURL.js
+++ src/GoogleAuthURL.js
@@ -2,7 +2,7 @@ import Oauth2BaseURL from "./Oauth2BaseURL";
 import Oauth2RedirectURL from "./Oauth2RedirectURI";
 
 const GoogleAuthURL =
-  Oauth2BaseURL +
+  "http://ec2-3-34-204-121.ap-northeast-2.compute.amazonaws.com:8080" +
   "/oauth2/authorization/google?redirect_uri=" +
   Oauth2RedirectURL;
 
--- src/NaverAuthURL.js
+++ src/NaverAuthURL.js
@@ -1,9 +1,8 @@
-import ApiBaseURL from "./ApiBaseURL";
 import Oauth2BaseURL from "./Oauth2BaseURL";
 import Oauth2RedirectURL from "./Oauth2RedirectURI";
 
 const NaverAuthURL =
-  Oauth2BaseURL +
+  "http://ec2-3-34-204-121.ap-northeast-2.compute.amazonaws.com:8080" +
   "/oauth2/authorization/naver?redirect_uri=" +
   Oauth2RedirectURL;
 
