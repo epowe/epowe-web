162f17050a3546e8efb73b40c3c8f2e6f94e9733
seoyoonOh <5happy1004kr@naver.com>
2022-08-26 16:26:42 +0900

    feat: 녹화된 영상 s3에 바로 업로드

--- src/views/Record.js
+++ src/views/Record.js
@@ -1,4 +1,6 @@
 import FileSaver from "file-saver";
+import { useState } from 'react';
+import AWS from "aws-sdk";
 
 const detectMimeType = () => {
   const mimeTypes = [
@@ -9,6 +11,7 @@ const detectMimeType = () => {
 
   for (let mimeType of mimeTypes) {
     if (MediaRecorder.isTypeSupported(mimeType)) {
+      console.log("mimeType: " + mimeType);
       return mimeType;
     }
   }
@@ -91,5 +94,80 @@ export const playStream = (videoElement, stream) => {
 
 export const download = (recordedBlobs, fileName = "RecordedVideo.webm") => {
   const blob = combineBlobs(recordedBlobs);
-  return FileSaver.saveAs(blob, fileName);
+  console.log("blob: " + blob);
+  const recordedFile = new File([blob], 'recordedVideo.webm', {
+    type: {detectMimeType},
+  });
+  console.log(recordedFile);
+  uploadFile(recordedFile);
+  // return FileSaver.saveAs(blob, fileName);
+};
+
+//엑세스키와 시크릿 키는 각자의 aws 계정에 IAM을 들어가고 내 보안 자격증명에 들어가서 본인만의 엑세스 키를 만들수 있다.
+
+const ACCESS_KEY = process.env.REACT_APP_AWS_ACCESS_KEY;
+const SECRET_ACCESS_KEY = process.env.REACT_APP_AWS_SECRET_ACCESS_KEY;
+//region을 써야한다.
+const REGION = process.env.REACT_APP_AWS_REGION;
+//s3 버킷명을 써야한다.
+const S3_BUCKET = process.env.REACT_APP_AWS_BUCKET;
+
+AWS.config.update({
+  accessKeyId: ACCESS_KEY,
+  secretAccessKey: SECRET_ACCESS_KEY,
+});
+
+const myBucket = new AWS.S3({
+  params: { Bucket: S3_BUCKET },
+  region: REGION,
+});
+
+//클릭시 S3에 업로드 되도록 만들어주는 함수
+const uploadFile = (file) => {
+  const fileExt = file.name.split(".").pop();
+  let today = new Date();
+  let year = today.getFullYear(); // 년도
+  let month = today.getMonth() + 1; // 월
+  let date = today.getDate(); // 날짜
+  let hours = today.getHours(); // 시
+  let minutes = today.getMinutes(); // 분
+  let seconds = today.getSeconds(); // 초
+  let milliseconds = today.getMilliseconds(); // 밀리초
+  let finalFileName =
+    year +
+    ":" +
+    month +
+    ":" +
+    date +
+    ":" +
+    hours +
+    ":" +
+    minutes +
+    ":" +
+    seconds +
+    ":" +
+    milliseconds +
+    "." +
+    fileExt;
+
+  const params = {
+    //엑세스 컨트롤
+    ACL: "public-read",
+    Body: file,
+    Bucket: S3_BUCKET,
+    //키는 파일명, 업로드라는 파일 명에 키 이름 생성
+    Key: "upload/" + finalFileName,
+  };
+
+  myBucket
+      .putObject(params)
+      .send((err) => {
+        if (err) console.log(err);
+      });
+      
+  const encodeFileName = encodeURIComponent(finalFileName);
+  const url =
+    "https://epowe-bucket.s3.ap-northeast-2.amazonaws.com/upload/" +
+    encodeFileName;
+  console.log(url);
 };
